9eed35d92c526caa229a8050e5d3d17100914b6b
NetaAviv <neta.aviv@nitzanim.tech>
2025-02-12 12:40:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,18 @@ def main():
         if instance_type not in ["t3.nano", "t4g.nano"]:
             print("Invalid instance type. Please choose either 't3.nano' or 't4g.nano'.")
         else:
-            valid_instance_type = True  # Fix typo here
-            print(f"You've selected {instance_type}. Proceeding with instance creation...")
+            valid_instance_type = True 
+            print(f"You've selected {instance_type}.")
+    
+    valid_os = False
+    while not valid_os:
+        os = input("Please choose an os for you instance, 'aws linux' or 'ubuntu'. ").strip()
+
+        if os not in ["aws linux", "ubuntu"]:
+            print("Invalid os. Please choose either 'aws linux' or 'ubuntu'.")
+        else:
+            valid_os = True
+            print(f"You've selected {os}. Proceeding with instance creation...")
 
 if __name__ == "__main__":
     main()
