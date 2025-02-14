45ca4d806e04c8cce79ff6bb211e66991891b514
NetaAviv <neta.aviv@nitzanim.tech>
2025-02-14 23:36:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,40 +11,8 @@ def main():
 
     try:
         if to_do == '1':
-            to_view = input("Enter 1 - View running instances\n"
-                            "Enter 2 - View stopped instances\n"
-                            "Enter 3 - View all\n"
-                            "Your input: ").strip()
-            
-            if to_view == '1':
-                running_instances = list_instances("running")
-                if len(running_instances) == 0:
-                    print("No running instances.")
-                else:
-                    print("The IDs of the instances you have running already:")
-                    for instance in running_instances:
-                        print(f"Instance ID: {instance.id}, State: {instance.state['Name']}")
-
-            elif to_view == '2':
-                stopped_instances = list_instances("stopped")
-                if len(stopped_instances) == 0:
-                    print("No stopped instances.")
-                else:
-                    print("The IDs of the stopped instances:")
-                    for instance in stopped_instances:
-                        print(f"Instance ID: {instance.id}, State: {instance.state['Name']}")
-
-            elif to_view == '3':
-                all_instances = list_instances("stopped") + (list_instances("running"))
-                if len(all_instances) == 0:
-                    print("No instances were made by the program.")
-                else:
-                    print("The IDs of all instances made by the program:")
-                    for instance in all_instances:
-                        print(f"Instance ID: {instance.id}, State: {instance.state['Name']}")
-            
-            else:
-                print("Invalid option for viewing instances.")
+            print("You chose to view instances made by the program.")
+            viewing_request()
 
         elif to_do == '2':
             print("You chose to launch a new EC2 instance.")
@@ -64,7 +32,6 @@ def main():
                 instance_id = input("Enter the ID of the instance you want to start: ").strip()
                 if any(instance.id == instance_id for instance in instances):
                     start_instance(instance_id)
-                    print(f"Started instance {instance_id}.")
                 else:
                     print(f"Invalid instance ID: {instance_id}")
             else:
@@ -81,7 +48,6 @@ def main():
                 instance_id = input("Enter the ID of the instance you want to stop: ").strip()
                 if any(instance.id == instance_id for instance in instances):
                     stop_instance(instance_id)
-                    print(f"Stopped instance {instance_id}.")
                 else:
                     print(f"Invalid instance ID: {instance_id}")
             else:
