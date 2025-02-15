ee456f04fb1fa964b5c2281fd7b3c088833cefac
NetaAviv <neta.aviv@nitzanim.tech>
2025-02-15 15:31:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,42 +19,18 @@ def main():
             elif to_do == '2':
                 print("You chose to launch a new EC2 instance.")
                 if len(list_instances("running")) >= 2:
-                    raise Exception("Cannot create more than 2 running instances at a time.")
+                      raise Exception("Cannot create more than 2 running instances at a time.")
                 else:
                     get_new_instance_details()
 
             elif to_do == '3':
                 print("You chose to start a stopped instance.")
-                instances = list_instances("stopped")
-                if len(instances) > 0:
-                    print("Your stopped instances are: ")
-                    for instance in instances:
-                        print(f"Instance ID: {instance.id}")
-
-                    instance_id = input("Enter the ID of the instance you want to start: ").strip()
-                    if any(instance.id == instance_id for instance in instances):
-                        start_instance(instance_id)
-                    else:
-                        print(f"Invalid instance ID: {instance_id}")
-                else:
-                    print("You don't have any stopped instances.")
+                starting_instance_request()
 
             elif to_do == '4':
                 print("You chose to stop a running instance.")
-                instances = list_instances("running")
-                if len(instances) > 0:
-                    print("Your running instances are: ")
-                    for instance in instances:
-                        print(f"Instance ID: {instance.id}")
+                stopping_instance_request()
 
-                    instance_id = input("Enter the ID of the instance you want to stop: ").strip()
-                    if any(instance.id == instance_id for instance in instances):
-                        stop_instance(instance_id)
-                    else:
-                        print(f"Invalid instance ID: {instance_id}")
-                else:
-                    print("You don't have any running instances.")
-            
             elif to_do == '5':
                 print("Exiting the program...")
                 stop_program = True
